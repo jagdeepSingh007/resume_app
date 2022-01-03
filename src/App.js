f0e623c649bdb16808d1a041a7431b09f3ea9cb2
Varunsingh5 <singhvarun9101998@gmail.com>
2022-01-03 15:23:25 +0530

    added bar component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Profile from './components/Profile/Profile'
 import Header from './components/Header/Header';
 import Portfolio from './pages/Portfolio/Portfolio';
 import Resume from './pages/Resume/Resume';
+import {Bar} from './components/Bar';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
@@ -23,7 +24,7 @@ function App() {
               <Route path = "/resume" element = { <Resume/> } />
             </Routes> 
           </Router>
-         
+         <Bar/>
           <Footer />
         </Grid>
 
---
+++ src/components/Bar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Bar = () => {
+    return (
+        <div>
+hello Bar
+        </div>
+    )
+}
